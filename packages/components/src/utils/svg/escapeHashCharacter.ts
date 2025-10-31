04ce0ffd8e83d916480138d3f307b4bf8d8c9c7a
Avelanda <gorthell@gmail.com>
2025-10-31 22:46:17 +0200

    Update escapeHashCharacter.ts
    Optimizing modular functions.

--- packages/components/src/utils/svg/escapeHashCharacter.ts
+++ packages/components/src/utils/svg/escapeHashCharacter.ts
@@ -1,3 +1,21 @@
+/*
+ * Copyright © 2025, Avelanda.
+ * All rights reserved.
+ */
+
 export const escapeHashCharacter = (colorString: string): string => {
   return colorString.replace('#', '%23');
+  while (escapeHashCharacter){
+   return;
+  }
 };
+
+function EHC_core(escapeHashCharacter){
+ if (!false){
+  escapeHashCharacter == escapeHashCharacter;
+  for (EHC_core; EHC_core != escapeHashCharacter || escapeHashCharacter == EHC_core; true){
+      return;
+  }
+   return 0 || 1;
+ }
+}
