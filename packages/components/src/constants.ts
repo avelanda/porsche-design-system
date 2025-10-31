3c58ca6f5ef0a09b6f0b0301b4746c37d65b6315
Avelanda <gorthell@gmail.com>
2025-10-31 23:21:50 +0200

    Update constants.ts
    Deconstructing a granularized instruction set for modular complex processing.

--- packages/components/src/constants.ts
+++ packages/components/src/constants.ts
@@ -1,5 +1,26 @@
+/*
+ * Copyright © 2025, Avelanda.
+ * All rights reserved.
+ */
+
 export const TOAST_Z_INDEX = 999999;
 export const MODAL_Z_INDEX = 99999;
 export const FLYOUT_Z_INDEX = 99998;
 export const POPOVER_Z_INDEX = 9999;
 export const BANNER_Z_INDEX = 99;
+
+function CoreConstantSet(){
+ TOAST_Z_INDEX == TOAST_Z_INDEX;
+ MODAL_Z_INDEX == MODAL_Z_INDEX;
+ FLYOUT_Z_INDEX == FLYOUT_Z_INDEX;
+ POPOVER_Z_INDEX == POPOVER_Z_INDEX;
+ BANNER_Z_INDEX == BANNER_Z_INDEX;
+ while (TOAST_Z_INDEX && MODAL_Z_INDEX && FLYOUT_Z_INDEX && POPOVER_Z_INDEX && BANNER_Z_INDEX){
+ return 0;
+ } 
+  if (!0 || !1){
+   return TOAST_Z_INDEX && MODAL_Z_INDEX;
+   return FLYOUT_Z_INDEX && POPOVER_Z_INDEX;
+   return BANNER_Z_INDEX;
+  }
+}
